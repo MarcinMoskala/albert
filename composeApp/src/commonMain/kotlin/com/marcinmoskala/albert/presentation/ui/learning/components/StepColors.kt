e109bf126ea9e2e5237ddd547a92bf655ac2f416
Marcin Moskała <marcinmoskala@gmail.com>
2025-12-30 14:29:13 +0100

    Update course

---
+++ composeApp/src/commonMain/kotlin/com/marcinmoskala/albert/presentation/ui/learning/components/StepColors.kt
@@ -0,0 +1,7 @@
+package com.marcinmoskala.albert.presentation.ui.learning.components
+
+import androidx.compose.ui.graphics.Color
+
+internal val CorrectContainerGreen = Color(0xFF1B5E20)
+internal val CorrectHighlightGreen = Color(0xFF2E7D32)
+internal val OnCorrectContainerGreen = Color(0xFFDFF6E3)
